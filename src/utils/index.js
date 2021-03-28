5d8eca038fef16fff58735536e0ebef72179e3c4
kunalpan <panchalkunal9498@gmail.com>
2021-03-28 05:54:06 +0530

    files edited design created

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 async function customFetch(URL, METHOD = 'get', config = null) {
-	const data = await axios({ url: URL, method: METHOD, ...config }).then(
-		(res) => res.data
-	);
+	const data = await axios({ url: URL, method: METHOD, ...config })
+		.then((res) => res.data)
+		.catch((err) => console.log(err));
 
 	return data;
 }
